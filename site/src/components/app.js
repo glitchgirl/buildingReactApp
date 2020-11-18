b64a25d30753077b7a2cb0fbaeaba8ae6e5cba21
glitchgirl <londonspleen@gmail.com>
2020-11-18 09:59:25 -0500

    making videolist

--- site/src/components/app.js
+++ site/src/components/app.js
@@ -1,12 +1,23 @@
 import React from "react";
 import SearchBar from "./searchBar";
-
+import youtube from "../apis/youtube";
+import VideoList from "./videolist";
 class App extends React.Component {
+  state = { videos: [] };
+  onTermSubmit = async term => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term
+      }
+    });
+
+    this.setState({ videos: response.data.items });
+  };
   render() {
     return (
       <div>
-        hewwo
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
---
+++ site/src/components/videolist.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const VideoList = props => {
+  return <div>{props.videos}</div>;
+};
+export default VideoList;
